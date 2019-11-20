c5468f882a284cc4e2234063a582d596d3614c66
zhuzhengjia <zhuzhengjia@bytedance.com>
2019-11-20 15:05:56 +0800

    log fix

--- WindowsUtilLib/log.cpp
+++ WindowsUtilLib/log.cpp
@@ -1,8 +1,15 @@
 #include "log.h"
-
+//Any cpp include log.h and one need to call FileLoggerInit.
 std::shared_ptr<spdlog::logger> file_logger;
+bool isfile_loggerInit = false;
+
 void FileLoggerInit(const char* fileName) {
+	if (isfile_loggerInit) {
+		return;
+	}
 	file_logger = spdlog::basic_logger_mt("log", fileName);
 	spdlog::set_default_logger(file_logger);
 	spdlog::flush_every(std::chrono::seconds(2));
+	isfile_loggerInit = true;
+	return;
 }
