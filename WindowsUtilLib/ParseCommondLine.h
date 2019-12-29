364080595a511db4aab15905aac32662b639fbd3
zhuzhengjia <zhuzhengjia@bytedance.com>
2019-12-29 17:33:08 +0800

    fix a warning

--- WindowsUtilLib/ParseCommondLine.h
+++ WindowsUtilLib/ParseCommondLine.h
@@ -4,7 +4,7 @@ template<class T>
 Vec<T> ParseCommondLine(int argc, T argv[]) {
 	Vec<T> args;
 
-	for (size_t i = 0; i < argc; i++) {
+	for (int i = 0; i < argc; i++) {
 		args.AppendStrPtr(argv[i]);
 	}
 	return args;
