a0d5dc2efaeacd6e29d9aa4a4bdfd346bbaebb08
zhuzhengjia <491694003>
2021-05-17 17:30:14 +0800

    dbg加入dump支持

--- WindowsUtilLib/CRTdbgHeader.h
+++ WindowsUtilLib/CRTdbgHeader.h
@@ -5,6 +5,98 @@
 #include <crtdbg.h>
 #endif // DEBUG
 
-/*
-	Visual Studio runtime error code support： Add $err,hr in watch window.
-*/
+
+#include <stdio.h>
+#include <time.h>
+#include <windows.h>
+#include <DbgHelp.h>
+#include <string>
+#pragma comment(lib, "DbgHelp.lib")
+
+static std::string GetExecutablePath()
+{
+    char current_proc_path[MAX_PATH] = {0};
+    ::GetModuleFileNameA(NULL, current_proc_path, MAX_PATH);
+
+    std::string exePath;
+    exePath.append(current_proc_path);
+    int separator_pos = exePath.rfind('\\');
+
+    if (std::string::npos == separator_pos)
+    {
+        exePath = "";
+    }
+    else
+    {
+        exePath = exePath.substr(0, separator_pos);
+    }
+    return exePath;
+}
+
+
+inline LONG WINAPI WriteDumpHandler(struct _EXCEPTION_POINTERS *pExceptionInfo)
+    {
+    // 返回EXCEPTION_CONTINUE_SEARCH，让程序停止运行
+    LONG ret = EXCEPTION_CONTINUE_SEARCH;
+
+    time_t nowtime;
+    time(&nowtime);
+    struct tm *pTime = localtime(&nowtime);
+    char params[128] = {0};
+    char current_proc_path[MAX_PATH] = {0};
+    ::GetModuleFileNameA(NULL, current_proc_path, MAX_PATH);
+
+    std::string fileName = current_proc_path;
+    // 设置core文件生成目录和文件名
+    sprintf(params, "%4d.%02d.%02d_%02d.%02d.%02d.dmp", pTime->tm_year + 1900, pTime->tm_mon + 1, pTime->tm_mday,
+            pTime->tm_hour, pTime->tm_min, pTime->tm_sec);
+    fileName += "_";
+    fileName += params;
+    HANDLE hFile =
+        ::CreateFileA(fileName.c_str(), GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile != INVALID_HANDLE_VALUE)
+    {
+        MINIDUMP_EXCEPTION_INFORMATION ExInfo;
+
+        ExInfo.ThreadId          = ::GetCurrentThreadId();
+        ExInfo.ExceptionPointers = pExceptionInfo;
+        ExInfo.ClientPointers    = NULL;
+
+        // write the dump
+        BOOL bOK =
+            MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hFile, MiniDumpNormal, &ExInfo, NULL, NULL);
+        ret = EXCEPTION_EXECUTE_HANDLER;
+        ::CloseHandle(hFile);
+    }
+
+    return ret;
+}
+
+inline BOOL PreventSetUnhandledExceptionFilter()
+{
+    HMODULE hKernel32 = LoadLibraryA("kernel32.dll");
+
+    if (hKernel32 == NULL)
+        return FALSE;
+
+    void *pOrgEntry = GetProcAddress(hKernel32, "SetUnhandledExceptionFilter");
+
+    if (pOrgEntry == NULL)
+        return FALSE;
+
+    unsigned char newJump[100];
+
+    DWORD dwOrgEntryAddr = (DWORD)pOrgEntry;
+
+    dwOrgEntryAddr += 5; // add 5 for 5 op-codes for jmp far
+
+    void *pNewFunc = &WriteDumpHandler;
+
+    DWORD dwNewEntryAddr = (DWORD)pNewFunc;
+    DWORD dwRelativeAddr = dwNewEntryAddr - dwOrgEntryAddr;
+    newJump[0]           = 0xE9; // JMP absolute
+    memcpy(&newJump[1], &dwRelativeAddr, sizeof(pNewFunc));
+    SIZE_T bytesWritten;
+    BOOL bRet = WriteProcessMemory(GetCurrentProcess(), pOrgEntry, newJump, sizeof(pNewFunc) + 1, &bytesWritten);
+    return bRet;
+}
